9535f1ca967d1c1fe746975cb46261fd0a1c5a30
NitinV2000 <nitin.vankadari2018@vitstudent.ac.in>
2021-11-23 19:26:17 +0530

    Added the dependencies

---
+++ src/main/java/com/example/projectGenerator/ProjectGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectGenerator/ProjectGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
